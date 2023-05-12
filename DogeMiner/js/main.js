b180464fed17dbf954ec1d0677613c5909ad3286
Zelofici <ansh@amogus.email>
2023-05-12 12:14:13 -0500

    DogeMiner added

---
+++ DogeMiner/js/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:334b955752da5b34993ea1315439f7bc369b418980229734ca2ef3419762c1c7
+size 631168
---
+++ DogeMiner/js/plugins.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:73aade9c66dbe6d5157cd1725c514dc8169ea7a1bad7a9c15273302f8041a393
+size 399804
